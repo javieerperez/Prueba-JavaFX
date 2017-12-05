f20c6893cb098dec62809b525868984155eb79c5
Javier <javieerperez@hotmail.com>
2017-12-05 18:28:04 +0100

    Segundo commit, funcionalidad de la navegacion

--- src/Control/ControlFX.java
+++ src/Control/ControlFX.java
@@ -1,17 +1,33 @@
 package Control;
 
+import java.io.IOException;
+
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ControlFX {
-	
+
+	public void cambiarEscena() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(MainFX.class.getResource("FreeSolo.fxml"));
+			BorderPane page = (BorderPane) loader.load();
+			Stage sendStage = new Stage();
+			sendStage.setTitle("FREESOLO");
+			Scene scene = new Scene(page);
+			sendStage.setScene(scene);
+			sendStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@FXML
-	Stage stage;
-	Scene scene2;
-	
-	@FXML
-	public void cambiarEscena() throws Exception {
-		stage.setScene(scene2);
+	private void handleSend() {
+		cambiarEscena();
 	}
 }
--- src/Control/MainFX.java
+++ src/Control/MainFX.java
@@ -1,5 +1,7 @@
 package Control;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,11 +10,12 @@ import javafx.stage.Stage;
 
 public class MainFX extends Application {
 	
+	
 	AnchorPane mypane;
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("../Vista/Login.fxml"));
+	public void start(Stage primaryStage) throws IOException{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
 		mypane = (AnchorPane) loader.load();
 		primaryStage.setTitle("Login");
 		primaryStage.setScene(new Scene(mypane));
